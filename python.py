c466a1a2e1971cd9b2d089f2647d4aac68458456
EC2 Default User <ec2-user@ip-172-31-42-252.ap-south-1.compute.internal>
2026-08-18 06:06:50 +0000

    adding file

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
-num=int(input("enter a number:"))
-if num %2==0:
-    print("even number")
-else:
-    print("odd number")
+for i in range (1,25):
+    if i %2==0:
+        print("even number")
+    else:
+        print("odd number")
 
